05463c5a4d5266a57d276fd823be431ca5a6a3ca
Simant Dhakal <Simant.dhakal@gmail.com>
2019-12-02 10:31:24 +0545

    new commit by simant dai

--- app/src/main/java/com/example/eventnepal/CityName.java
+++ app/src/main/java/com/example/eventnepal/CityName.java
@@ -1,4 +1,4 @@
-package com.simant.sportfanatic;
+package com.example.eventnepal;
 
 public class CityName {
     public static final String[] cityNames = {"Kathmandu", "Bhaktapur", "Lalitpur",
--- app/src/main/java/com/example/eventnepal/CreateTeamActivity.java
+++ app/src/main/java/com/example/eventnepal/CreateTeamActivity.java
@@ -1,7 +1,19 @@
 package com.example.eventnepal;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.mikhaellopez.circularimageview.CircularImageView;
 
 public class CreateTeamActivity extends AppCompatActivity {
 
@@ -9,5 +21,7 @@ public class CreateTeamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_team);
+
     }
+
 }
